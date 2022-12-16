d1c4dc7839ca2741e2c67980bab4482ee82a3735
golushcov <120759435+golushcov@users.noreply.github.com>
2022-12-17 02:20:38 +0300

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,12 @@
+BOT_TOKEN_FILENAME = 'token.txt'
+
+SYMBOL_X = '❌'
+SYMBOL_O = '⭕'
+SYMBOL_UNDEF = '◻'
+
+ANSW_YOUR_TURN = 'Ваш ход'
+ANSW_YOU_WIN = 'Вы победили'
+ANSW_BOT_WIN = 'Победил бот'
+ANSW_DRAW = 'Ничья'
+
+ALERT_CANNOT_MOVE_TO_THIS_CELL = 'Нажимать можно только на ' + SYMBOL_UNDEF
